3c2b8a090b04dbbd531f4ed54e14c7926d02181a
MOWENRAN <2513174679@qq.com>
2019-11-26 20:14:48 +0800

    程序员A  基地及

--- src/main/java/com/zking/test.java
+++ src/main/java/com/zking/test.java
@@ -2,6 +2,10 @@ package com.zking;
 
 public class test {
     public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int c=a+b;
+
         System.out.println("hollow");
     }
 }
